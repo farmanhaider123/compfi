0547b8b304544a3f1553d4abc73b4a6e162effb4
farmanhaider <farmanhaider2402gmail.com git config --global user.email farmanhaider2402gmail.com>
2023-01-09 16:48:41 +0530

    first commit

---
+++ Backend/Model/constants.js
@@ -0,0 +1,4 @@
+module.exports.USER = 'user';
+module.exports.ADMIN = 'admin';
+module.exports.EXECUTIVE = "executiveUser";
+module.exports.FIELDEXECUTIVE = "fieldExecutiveUser";
---
+++ Backend/Model/users.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
+const { ADMIN, USER,EXECUTIVE,FIELDEXECUTIVE } = require("./constants");
+const { boolean } = require("joi");
+
+const User = mongoose.model(
+  "user",
+  new mongoose.Schema({
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 25,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 25,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      minLength: 5,
+      maxLengh: 155,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 8,
+      },
+    
+    role: {
+      type: String,
+      enum: [ADMIN, USER,EXECUTIVE,FIELDEXECUTIVE],
+      default: USER,
+    },
+    contactNumber: {
+      type: Number,
+      min: 1000000000,
+      unique: true,
+    },
+    status: {
+      type: Boolean,
+      default: false,
+      },
+    otCreatedAt:DateTime
+    
+  }, { timestamps: true })
+);
+
+const validateUser = (user) => {
+  const userSchema = Joi.object({
+    firstName: Joi.string().min(0).max(50).required(),
+    lastName: Joi.string().min(0).max(50).required(),
+    password: Joi.string().min(5).max(50).required(),
+    email: Joi.string().email().min(5).max(255).required(),
+    contactNumber: Joi.string().min(10).max(10).required(),
+  });
+
+  const validationResult = userSchema.validate(user);
+  return validationResult;
+};
+
+const validatePassword = ({ name, password }) => {
+  if (password.toLowerCase().includes(name.toLowerCase())) {
+    return { error: "Password cannot contain username" };
+  }
+  return passwordComplexity().validate(password);
+};
+
+module.exports = { User, validateUser, validatePassword };
